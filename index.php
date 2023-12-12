3c31bc8e86955b69d2cbba8043a0205d83760aa8
Osborn Maja <98744068+Bornmajor@users.noreply.github.com>
2023-12-12 17:31:54 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
+<?php 
+if(isset($_GET['component'])){
+$component = $_GET['component'];
+}else{
+$component = '';
+}
 
-<meta name="description" content="Polling system">
-<title>Polls</title>
-</head>
-<body>
+switch($component){
+case 'home';
+include('components/home.php');
+break;
+case 'login';
+include('components/login.php');
+break;
+case 'registration';
+include('components/registration.php');
+break;
+case 'manage_poll';
+include('components/manage_poll.php');
+break;
+case 'registration_poll';
+include('components/registration_poll.php');
+break;
+case 'voting';
+include('components/voting.php');
+break;
+case 'signout';
+include('components/signout.php');
+break;
+default:
+include('components/home.php');
+
+}
+
+?>
+<?php include('includes/footer.php'); ?>
     
 
    
